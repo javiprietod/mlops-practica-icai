6919d9b13b4c2a0f7a9c6ae815ddb82aef2b8e68
javiprietod <javierpridom@gmail.com>
2025-09-26 08:25:36 +0200

    Added .env, seaborn and changed train.py

--- train.py
+++ train.py
@@ -1,19 +1,26 @@
+import os
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 import joblib
 import mlflow
 import mlflow.sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-import dagshub
-dagshub.init(repo_owner='javiprietod', repo_name='mlops-practica-icai', mlflow=True)
+tracking_uri = os.environ.get("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(tracking_uri)
 
-# Cargar el conjunto de datos
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+# Cargar el conjunto de datos desde el archivo CSV
+try:
+ iris = pd.read_csv('data/iris_dataset.csv')
+except FileNotFoundError:
+ print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
+# Dividir el DataFrame en características (X) y etiquetas (y)
+X = iris.drop('target', axis=1)
+y = iris['target']
 # Iniciar un experimento de MLflow
 with mlflow.start_run():
     # Dividir los datos en conjuntos de entrenamiento y prueba
@@ -39,3 +46,14 @@ with mlflow.start_run():
     mlflow.log_metric("accuracy", accuracy)
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
+    # --- Sección de Reporte para CML ---
+    # 1. Generar la matriz de confusión
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title('Matriz de Confusión')
+    plt.xlabel('Predicciones')
+    plt.ylabel('Valores Reales')
+    plt.savefig('confusion_matrix.png')
+    print("Matriz de confusión guardada como 'confusion_matrix.png'")
+    # --- Fin de la sección de Reporte ---
